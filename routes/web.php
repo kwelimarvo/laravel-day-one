9ed4ded083bd0d78f85a7a74520241189065f410
kwelimarvo <marvin@kwelicapital.com>
2022-06-10 17:12:58 +0300

    Service Container

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+class Services
+{
+    public $name = '';
+}
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/service', function (Services $service) {
+    die(get_class($service));
 });
